ed956ffd6fb73c67a58306ecb85ceda38b8ff654
TheWebDevel <sathish@skcript.com>
2020-05-21 23:21:34 +0400

    tested file IO

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // Go will automatically call this function
 //  with a parameter t *testing.T Here, the t is our
@@ -14,4 +17,33 @@ func TestNewDeck(t *testing.T) {
 		// Notify the test handler
 		t.Errorf("Expected deck length of 16 but got %v", len(d))
 	}
+
+	// Verify first element of the deck
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	// Verify last element of the deck
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs but got %v", d[0])
+	}
+}
+
+// A function to test save deck to file
+//  and load deck from file
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// Remove files
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16 but got %v", len(loadedDeck))
+	}
+
+	// Remove files
+	os.Remove("_decktesting")
 }
